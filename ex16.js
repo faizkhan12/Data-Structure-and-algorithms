28c6d3fbc4337ef9b2ef1b8476d6a7cdf28efa7e
Faiz Khan <faizk6797@gmail.com>
2018-07-22 00:32:52 +0530

    Basic algorithms

---
+++ ex16.js
@@ -0,0 +1,12 @@
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  var groups=[];
+  while(arr.length>0)
+  {
+    groups.push(arr.slice(0,size))
+    arr=arr.slice(size);
+  }
+  return groups;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
